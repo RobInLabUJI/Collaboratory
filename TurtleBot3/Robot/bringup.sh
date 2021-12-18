e1668ee4dc88d8c85247bdc51370989aa124e737
ecervera <Enric Cervera>
2021-12-18 17:58:24 +0100

    Add bringup script

---
+++ TurtleBot3/Robot/bringup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
+
+export ROS_HOSTNAME=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
+
+roslaunch turtlebot3_bringup turtlebot3_robot.launch &
